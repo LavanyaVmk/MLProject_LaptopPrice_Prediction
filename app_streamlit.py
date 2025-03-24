1e6484133a1b258d0f2631d219a529190c792003
Lavanya Vishnumolakala <vmk.lavanya@gmail.com>
2025-03-24 16:42:49 +0530

    c

--- app_streamlit.py
+++ app_streamlit.py
@@ -28,7 +28,7 @@ st.markdown(f"""
         /* Adjust main container to shift content left/right */
         .block-container {{
             padding-left: -5% !important;
-            padding-right: 45% !important;
+            padding-right: 50% !important;
         }}
 
         /* Title container => center on page (no extra top margin) */
@@ -41,7 +41,7 @@ st.markdown(f"""
         /* Title box => center itself, 50% screen width */
         .title-box {{
             display: inline-block;
-            width: 55%;
+            width: 70%;
             background-color: #fffd37;
             color: #1900ff;
             font-size: 36px;
