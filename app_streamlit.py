f1cf3977ea66b200572cbccedb0cce1f46b0123c
Lavanya Vishnumolakala <vmk.lavanya@gmail.com>
2025-03-24 15:20:58 +0530

    c

--- app_streamlit.py
+++ app_streamlit.py
@@ -24,7 +24,6 @@ background_image_url = "https://github.com/LavanyaVmk/Laptop-Price-Prediction-ML
 
 /* Title container pinned to the left, no top margin */
 .title-container {{
-    margin-top: 20px !important;
     margin-left: 20px;
     z-index: 9999;
 }}
