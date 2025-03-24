b1123872680ce3937e61b14f224f3ee902e0f32f
Lavanya Vishnumolakala <vmk.lavanya@gmail.com>
2025-03-24 17:55:16 +0530

    c

--- app_streamlit.py
+++ app_streamlit.py
@@ -44,14 +44,14 @@ st.markdown(f"""
 
         /* Remove default Streamlit padding/margins */
         .block-container {{
-            padding-left: 8% !important;
+            padding-left: 7% !important;
             padding-right: 45% !important;
         }}
 
         /* Title container pinned to the very left, no extra top margin */
         .title-container {{
             margin-top: 15px;
-            margin-left: 10px;
+            margin-left: 0px;
             text-align: center;
             margin-bottom: 20px;
 
